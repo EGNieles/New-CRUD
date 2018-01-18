39cc408ad92a4a6af37e4fc9b035df56df303c4d
Ginel <ginelexaltado@gmail.com>
2018-01-19 02:39:32 +0800

    new push

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,11 +6,11 @@
 export const environment = {
   production: false,
   firebase: {
-  	apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: ""
+  	apiKey: "AIzaSyByL3Xamz2hobhzip9tBFcYeFtn8WZjuF8",
+    authDomain: "new-u-a7992.firebaseapp.com",
+    databaseURL: "https://new-u-a7992.firebaseio.com",
+    projectId: "new-u-a7992",
+    storageBucket: "new-u-a7992.appspot.com",
+    messagingSenderId: "748338538119"
   }
 };
